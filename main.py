98ed6c9cfe0518c33975ac654af221f5dce8364e
Gaurav Bhujang <62141675+iamgauravbhujang@users.noreply.github.com>
2022-05-23 22:59:38 +0530

    File Updated

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Packages need for this project
 from secrets import choice
 from tkinter import *
 from PIL import Image, ImageTk
@@ -6,12 +7,13 @@ from random import randint
 
 window = Tk()
 
+# Giving the title
 window.title("Game Rock paper and scissor")
 
-
-
+# Set in the default background color
 window.configure(background="black")
  
+ # Assiging the image to variable
 image_rock = ImageTk.PhotoImage(Image.open("rock1.jpg"))
 image_paper = ImageTk.PhotoImage(Image.open("paper1.jpg"))
 image_scissor = ImageTk.PhotoImage(Image.open("scissor1.jpg"))
@@ -25,6 +27,7 @@ label_computer = Label(window, image=image_scissor1)
 label_computer.grid(row=1, column=0)
 label_player.grid(row=1, column=4)
 
+# Creating the variable which update when computer/player wins
 computer_score = Label(window, text=0, font=("arial",60,"bold"), fg="red")
 player_score = Label(window, text=0, font=("arial",60,"bold"), fg="red")
 computer_score.grid(row=1, column=1)
@@ -81,14 +84,15 @@ def winner_check(p,c):
 
 to_select = ['rock', 'paper', 'scissor']
 
+# On the basis of player choice it'll call winner_check() 
 def choice_update(a):
     choice_computer = to_select[randint(0,2)]
     if choice_computer == 'rock':
-        label_computer .configure(image=image_rock1)
+        label_computer.configure(image=image_rock1)
     elif choice_computer == 'paper':
-        label_computer .configure(image=image_paper1)
+        label_computer.configure(image=image_paper1)
     else:
-        label_computer .configure(image=image_scissor1)
+        label_computer.configure(image=image_scissor1)
 
     if a=="rock":
         label_player.configure(image=image_rock)
@@ -103,11 +107,12 @@ def choice_update(a):
 final_message = Label(window, font=("arial", 40, "bold"), bg="red", fg="white")
 final_message.grid(row=3, column=2)
 
-
+# Creating button 
 button_rock = Button(window, width=16, height=3, text="Rock", font=("arial",20,"bold"), bg="yellow", fg="red", command=lambda:choice_update("rock")).grid(row=2, column=1)
 
 button_paper = Button(window, width=16, height=3, text="Paper", font=("arial",20,"bold"), bg="yellow", fg="red",command=lambda:choice_update('paper')).grid(row=2,column=2)
 
 button_scissor = Button(window, width=16, height=3, text="Scissor", font=("arial",20,"bold"), bg="yellow", fg="red", command=lambda:choice_update('scissor')).grid(row=2,column=3)
 
+# It tells Python to run Tkinter event loop
 window.mainloop()
